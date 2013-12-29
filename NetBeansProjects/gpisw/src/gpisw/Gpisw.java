3798cbba44858339313c4ceee0552add306cdb94
Marilyn <Marilyn@Marilyn-PC>
2013-12-29 04:40:55 -0300

    puede funcionar

---
+++ NetBeansProjects/gpisw/src/gpisw/Gpisw.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gpisw;
+
+/**
+ *
+ * @author Marilyn
+ */
+public class Gpisw {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
