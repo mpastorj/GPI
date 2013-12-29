51707075111875e5579711dc748c0e02e7a33c8a
Marilyn <Marilyn@Marilyn-PC>
2013-12-29 16:48:18 -0300

    Ventana Principal

--- NetBeansProjects/swgpi/src/swgpi/Swgpi.java
+++ NetBeansProjects/swgpi/src/swgpi/Swgpi.java
@@ -15,5 +15,7 @@ public class Swgpi {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Principal v = new Principal();
+        v.setVisible(true);
     }
 }
